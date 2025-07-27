42601fa0ca88c9045ec25caa3edc4f2c46f20d63
aditi291soni <aditi291soni@gmail.com>
2025-07-27 20:52:31 +0530

    Reverse of a no

--- Main.java
+++ Main.java
@@ -4,20 +4,14 @@ public class Main{
        Scanner in=new Scanner(System.in);
        System.out.println("Enter number");
        int n =in.nextInt(); //no of didgit
-        System.out.println("Enter number whose no of occurance want to find");
-        int b =in.nextInt();
-    // int n=53567;
-    // int b=5;
-       int count=0;
-       
-       while(n>0){
-        int p=n%10;
-        if(p==b){
-            count++;
-   
-        }
+    // int n=321;
+       int total=0;
+     while(n>0){
+        int rem=n%10;
         n=n/10;
-       }
-       System.out.println("No of occurance is"+count);
+        total=total*10+rem;
+     }
+     System.out.println("Reverse of no is "+total);
+     
      }
 }
