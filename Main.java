76bebe15577b98b25645b7cc47c8e3365f299d19
aditi291soni <aditi291soni@gmail.com>
2025-07-27 20:39:53 +0530

    no of occurance

--- Main.java
+++ Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
 public class Main{
      public static void main(String[] args){
-        System.out.println("hello world");
+       Scanner in=new Scanner(System.in);
+       System.out.println("Enter number");
+       int n =in.nextInt(); //no of didgit
+        System.out.println("Enter number whose no of occurance want to find");
+        int b =in.nextInt();
+    // int n=53567;
+    // int b=5;
+       int count=0;
+       
+       while(n>0){
+        int p=n%10;
+        if(p==b){
+            count++;
+   
+        }
+        n=n/10;
+       }
+       System.out.println("No of occurance is"+count);
      }
 }
